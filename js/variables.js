6461b89b2a9c569b51b4797f80d063ff4b80cdf4
Agu <agustintgiaccone1999@gmail.com>
2022-07-26 19:39:35 -0300

    borra archivos inecesarios

--- js/variables.js
+++ js/variables.js
@@ -1,32 +1,32 @@
 
-toastSA ("BIENVENIDO A MI PAGINA WEB")
+// toastSA ("BIENVENIDO A MI PAGINA WEB")
 
-let NombreCompleto
-let nombre = "coder"
-let apellido = "house"
-let esp = " "
-let años = 21
-let año = 2021
-let plataforma = nombre + apellido
-const curso = "javascrips"
+// let NombreCompleto
+// let nombre = "coder"
+// let apellido = "house"
+// let esp = " "
+// let años = 21
+// let año = 2021
+// let plataforma = nombre + apellido
+// const curso = "javascrips"
 
-let Nro1 = 20
-let Nro2 = 60
+// let Nro1 = 20
+// let Nro2 = 60
 
-let resultado1 = Nro1 / Nro2
-let resultado2
+// let resultado1 = Nro1 / Nro2
+// let resultado2
 
-let NombreUsuario = prompt("INGRESA TU NOMBRE")
-let apellidoUsuario = prompt("INGRESE SU APELLIDO")
-NombreCompleto = NombreUsuario + esp + apellidoUsuario
-alert("Bienvenido: " + NombreUsuario + esp + apellidoUsuario)
+// let NombreUsuario = prompt("INGRESA TU NOMBRE")
+// let apellidoUsuario = prompt("INGRESE SU APELLIDO")
+// NombreCompleto = NombreUsuario + esp + apellidoUsuario
+// alert("Bienvenido: " + NombreUsuario + esp + apellidoUsuario)
 
-resultado2 = confirm("Estas seguros que deseas continuar?")
+// resultado2 = confirm("Estas seguros que deseas continuar?")
 
-console.log("Hola: ", plataforma)
-console.info("nombre ingresado:", NombreCompleto)
-console.warn("este mensaje requiere de atencion")
-console.error("ahora si estas en problemas")
+// console.log("Hola: ", plataforma)
+// console.info("nombre ingresado:", NombreCompleto)
+// console.warn("este mensaje requiere de atencion")
+// console.error("ahora si estas en problemas")
 
 const productos = ['Hamburguesa Rellena De Queso Envuelta En Tocino', 'Brochet de hamburguesa', 'Hamburguesa de mar y tierra', 'Hamburguesa a la parrilla con huevo estrellado', 'Hamburguesa con Salsa de 3 Quesos', 'Hamburguesa hawaiana']
 const carrito = []
