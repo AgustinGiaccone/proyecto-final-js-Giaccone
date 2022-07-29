ccaefcb7bb57649a80e40ef9499ef727b90a35bd
Agu <agustintgiaccone1999@gmail.com>
2022-07-29 20:35:37 -0300

    Modifica el carrito para que quede superpuesto sobre las tarjetas

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+/*
+se inicializan las constantes y variables
+*/
 const lista = document.querySelector('#listado')
 var carritoAbierto = false
 var total = 0
@@ -19,7 +22,7 @@ const botonCerrar = document.getElementById('cerrarCarrito')
     botonCerrar.addEventListener("click", ()=> {
         document.getElementById('carrito').style.display = 'none'; })
 
-/* recorre el array que obtine de productos.json,
+/* Recorre el array que obtine de productos.json,
 crea la tarjeta utilizando los parametros de cada producto,
 agrega eventos de click al elemento a "agregar al carrito"
 que llama a la funcion agregarAlCarrito() pasando como parametro
@@ -51,17 +54,7 @@ const mostrarProductos = async () => {
             comprar.innerText = "Agregar al carrito"
             comprar.id = "pedido"
             comprar.addEventListener("click", ()=> {
-                agregarAlCarrito(`${producto.nombre}`,`${producto.precio}`,`${producto.emoji}`),
-                Swal.fire({
-                    toast: true,
-                    icon: 'success',
-                    title: 'Se agrego una/ '+`${producto.nombre}`+' al carrito',
-                    position: 'top',
-                    timer: 1500,
-                    showConfirmButton: false,
-                    background:'rgb(12, 99, 70)',
-                    color: 'black'
-            })} )
+                agregarAlCarrito(`${producto.nombre}`,`${producto.precio}`,`${producto.emoji}`)})
             row.appendChild(fourColumns)
             fourColumns.appendChild(card)
             card.appendChild(img)
@@ -77,12 +70,13 @@ const mostrarProductos = async () => {
     })
 }
 /*
-recibe como argumento el nombre, precio y emoji del producto
+Recibe como argumento el nombre, precio y emoji del producto
 cuando se agrega al carrito un producto.
 suma el costo del producto, lo agrega a la array carrito,
 actualiza el contador y crea el elemtento HTML en el div carrito
 */
 function agregarAlCarrito(producto,precio,emoji) {
+    mensajeAgregarCarrito(producto)
     sumarTotal(parseInt(precio))
     carrito.push(producto)
     contadorCarrito()
@@ -96,7 +90,7 @@ function agregarAlCarrito(producto,precio,emoji) {
 })}
 
 /*
-elimina el producto selecionado en el carrito,
+Elimina el producto selecionado en el carrito,
 y descuenta el precio del producto eliminado
 */
 function removerDelCarrito(producto,precio) {
@@ -109,7 +103,11 @@ function removerDelCarrito(producto,precio) {
         sumarTotal(0-precio)
         }
 }
-
+/*
+Modifica el atrubuto innertex del elemento con el id contadorCarrito
+por la cantidad de elementos que tiene el array carrito.
+Ejecuta las funciones que validan la cantidad de elementos en el carrito.
+*/
 function contadorCarrito(){
     document.getElementById('contadorCarrito').innerText = carrito.length;
     comprarBoton()
@@ -118,7 +116,9 @@ function contadorCarrito(){
 }
 
 /*
-se define el boton comprar los producto generenado un sweetalert
+Muestra en pantalla el elemento id botonComprar
+cuando se encuentra algun elemento en el arrry carrito, y
+genera un mensaje flotante al precionar este boton
 */
 function comprarBoton(){
     const sweetComprar = document.getElementById('botonComprar')
@@ -129,17 +129,20 @@ if(carrito.length > 0){
 }
 sweetComprar.addEventListener('click', function(){
     Swal.fire({
-        toast: true,
+        toast: false,
         icon: 'success',
-        title: 'Gracias por su compra espero que la difrute',
+        title: "Gracias por su compra, espero que la difrute",
         position: 'top',
-        // timer: 2500,
         background:'rgb(12, 99, 70)',
-        color: 'white'
-    })})
-}
+        color: 'white',
+        confirmButtonText: "Confirmar",
+        preConfirm: () => {location.reload()
+            }})
+        }
+    )}
 /*
-se define el borton vaciar que se ejeuta junto con la funcion vaciarCarrito()
+Muestra en pantalla el elemento id botonVaciar
+cuando se encuentra algun elemento en el arrry carrito
 */
 function botonVaciar(){
     const sweetVaciar = document.getElementById('botonVaciar')
@@ -150,26 +153,30 @@ if(carrito.length > 0){
 }
 }
 /*
-elimina todo los productos que se encuantran el el carrito
+Recorre el array carrito y elimina cada uno de los elementos del DOM
+que tienen como id el nombre del producto al iterar.
+Vacia el array carrito, vacia la variable total y ejecuta contador
+carrito para actualizar el contador.
 */
 function vaciarCarrito(){
     carrito.forEach(producto => {
         document.getElementById(`${producto}`).remove()
-        carrito=[]
     }
     )
+    carrito=[]
     total=0
     contadorCarrito()
 }
 /*
-suma los precios de los productos selecionados
+suma los precios de los productos selecionados y lo modifica en el DOM.
 */
 function sumarTotal(precio){
     total = total+precio
     document.getElementById("total").innerText="Total: $"+total
 }
 /*
-se muestra en pantalla el precio obtenidos por los productos siempre y cuando se encuenten en el carrito
+Muestra en pantalla el elemento html "precio" cuando
+el array carrito es mayor a cero
 */
 function preciototal(){
     const total = document.getElementById('total')
@@ -178,6 +185,28 @@ if(carrito.length > 0){
 }else{
     total.style.display = 'none'
 }
+}
+/*
+Muestra un mensaje flotante al agregar un producto al carrito.
+*/
+function mensajeAgregarCarrito(producto){
+    let unidad = ""
+    //se valida que producto se compra para usar el articulo correcto.
+    if(producto == "Papas"){
+        unidad = "unas"
+    } else{
+        unidad = "una"
+    }
+    Swal.fire({
+        toast: true,
+        icon: 'success',
+        title: 'Se agrego '+ unidad + " " + producto.toLowerCase() +' al carrito',
+        position: 'top',
+        timer: 1500,
+        showConfirmButton: false,
+        background:'rgb(12, 99, 70)',
+        color: 'white'
+})
 }
 
 mostrarProductos()
